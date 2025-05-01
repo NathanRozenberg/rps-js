2510c55441450b32910c1ea472adefa4bb895c94
NathanRozenberg <nathanlrozenberg@gmail.com>
2025-05-01 10:04:49 -0700

    Play Round Built Out

--- script.js
+++ script.js
@@ -22,3 +22,48 @@ function getHumanChoice(){
         return choice;
     }
 }
+
+function playRound(){
+    const humanChoice = getHumanChoice().toLowerCase()
+    const computerChoice = getComputerChoice()
+
+    if (humanChoice === computerChoice){
+        console.log("It's a tie! You both picked " + humanChoice[0].toUpperCase() + humanChoice.slice(1));
+    }
+
+    else if (humanChoice === "rock"){
+        if (computerChoice === "paper"){
+            console.log("You lose! Paper covers Rock.");
+            computerScore++;
+        }
+        else{
+            console.log("You win! Rock smashes Scissors.");
+            humanScore++;
+        }
+    }
+    else if (humanChoice === "paper"){
+        if (computerChoice === "rock"){
+            console.log("You win! Paper covers Rock.");
+            humanScore++;
+        }
+        else {
+            console.log("You lose! Scissors cuts Paper.");
+            computerScore++;
+        }
+    }
+    else if (humanChoice === "scissors"){
+        if (computerChoice === "rock"){
+            console.log("You lose! Rock smashes Scissors.");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Scissors cuts Paper.")
+            humanScore++;
+        }
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound()
